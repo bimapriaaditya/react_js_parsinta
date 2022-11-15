682a04cdffa572ee59dce8216c687a532f9550c2
bimapriaaditya <bpabima@gmail.com>
2022-11-15 20:42:59 +0700

    react props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import { IconBrandFacebook, IconBrandTwitter } from '@tabler/icons';
+
 export default function App() {
   return (
-    <div className={'bg-blue-600 grid place-content-center min-h-screen'}>
-      <h1 className={'text-xl'}>Lorem Ipsum</h1>
+    <div className={'bg-slate-600 grid place-content-center min-h-screen'}>
+      <div className={'flex gap-x-2'}>
+        <Button
+          type='button'
+          onClick={() => console.log('Facebook has clicked')}
+        >
+          <IconBrandFacebook />
+          Facebook
+        </Button>
+        <Button
+          type='submit'
+          onClick={() => console.log('Twitter has clicked')}
+        >
+          <IconBrandTwitter />
+          Twitter
+        </Button>
+      </div>
     </div>
+  );
+}
+IconBrandTwitter;
+function Button(props) {
+  const { text, children } = props;
+  return (
+    <button
+      {...props}
+      className={
+        '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 bg-blue-600 text-white px-4 py-2 rounded flex gap-x-2'
+      }
+    >
+      {text || children}
+    </button>
   );
 }
