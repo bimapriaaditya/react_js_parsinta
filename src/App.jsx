ea0374a6f3b3f535717f3d6c6370b814664a85ec
bimapriaaditya <bpabima@gmail.com>
2022-11-16 15:28:40 +0700

    update prettier rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import {
-  IconBrandFacebook,
-  IconBrandGithub,
-  IconBrandTwitter,
-} from '@tabler/icons';
+import { IconBrandFacebook, IconBrandGithub, IconBrandTwitter } from '@tabler/icons';
 import clsx from 'clsx';
 
 export default function App() {
   return (
     <div className={'bg-slate-600 grid place-content-center min-h-screen'}>
       <div className={'flex gap-x-2'}>
-        <Button
-          type='button'
-          onClick={() => console.log('Facebook has clicked')}
-        >
+        <Button type='button' onClick={() => console.log('Facebook has clicked')}>
           <IconBrandFacebook />
           Facebook
         </Button>
-        <Button
-          type='submit'
-          className={'bg-sky-600 text-white'}
-          onClick={() => console.log('Twitter has clicked')}
-        >
+        <Button type='submit' className={'bg-sky-600 text-white'} onClick={() => console.log('Twitter has clicked')}>
           <IconBrandTwitter />
           Twitter
         </Button>
-        <Button
-          onClick={() => console.log('Github has clicked')}
-          className={'bg-black text-white'}
-        >
+        <Button onClick={() => console.log('Github has clicked')} className={'bg-black text-white'}>
           <IconBrandGithub />
           Twitter
         </Button>
@@ -37,21 +23,9 @@ export default function App() {
 }
 IconBrandTwitter;
 function Button(props) {
-  const {
-    text,
-    children,
-    type = 'button',
-    className = 'bg-blue-600 text-white',
-  } = props;
+  const { text, children, type = 'button', className = 'bg-blue-600 text-white' } = props;
   return (
-    <button
-      {...props}
-      type={type}
-      className={clsx(
-        className,
-        '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 px-4 py-2 rounded flex gap-x-2'
-      )}
-    >
+    <button {...props} type={type} className={clsx(className, '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 px-4 py-2 rounded flex gap-x-2')}>
       {text || children}
     </button>
   );
