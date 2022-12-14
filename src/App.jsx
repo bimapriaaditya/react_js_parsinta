4fb92834dc19dad6eb38c9cd319bed7db5b701db
bimapriaaditya <bpabima@gmail.com>
2022-12-14 16:10:42 +0700

    useState & useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,53 @@
+import { useState, useRef } from 'react';
 import Button from './components/Buttons/Button';
 import Card from './components/Cards/Card';
 
 export default function App() {
+  let [name, setName] = useState('lorem');
+  const first_name = useRef('');
+  const last_name = useRef('');
+
+  const ChangeName = () => {
+    const firstName_value = first_name.current.value;
+    const lastName_value = last_name.current.value;
+    const fullname = firstName_value + ' ' + lastName_value;
+
+    setName(fullname);
+    first_name.current.value = firstName_value;
+    last_name.current.value = lastName_value;
+
+    console.log(fullname);
+  };
+
   return (
     <div className={'bg-slate-100 min-h-screen flex justify-center items-center'}>
-      <div className={'flex gap-x-2 max-w-xl w-full'}>
-        <Card>
-          <Card.Header>Lorem Ipsum</Card.Header>
-          <Card.Body>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet natus consequatur deserunt ducimus deleniti
-              officiis aliquam accusantium illum excepturi ex tempora maxime, est rerum vitae? Placeat harum quam
-              consequuntur reprehenderit?
-            </p>
-          </Card.Body>
-          <Card.Footer>
-            <Button>Login</Button>
-          </Card.Footer>
-        </Card>
+      <div className={'max-w-xl w-full'}>
+        <h3 className={'text-4xl block mb-3'}>Hello {name}</h3>
+        <div className='flex gap-3'>
+          <div>
+            <input
+              type='text'
+              name='change_name'
+              id='id_change_name'
+              placeholder='first name'
+              className='px-4 py-2 border border-slate-300 mb-4 outline-none focus:border-sky-300 focus:shadow-sm'
+              ref={first_name}
+            />
+            <input
+              type='text'
+              name='change_name'
+              id='id_change_name'
+              placeholder='last name'
+              className='px-4 py-2 border border-slate-300 mb-4 outline-none focus:border-sky-300 focus:shadow-sm'
+              ref={last_name}
+            />
+          </div>
+          <div>
+            <Button onClick={ChangeName} value-target=''>
+              Change Name
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
